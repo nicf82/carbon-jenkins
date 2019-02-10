a2398236bc8bdfaf1616f066a8f99a6621960618
Nic Fellows <nic@carboninternet.net>
2019-02-10 18:32:12 +0000

    Initial commit

---
+++ jobs.groovy
@@ -0,0 +1,58 @@
+
+def sites = [
+    "www.carboninternet.net",
+    "service.carboninternet.net",
+    "www.vandervelde.co.uk",
+    "www.amboxingnewcastle.co.uk",
+    "www.flightsimcentre.com",
+    "www.blueyonderairtours.com",
+    "www.combat-athletics.co.uk",
+    "www.funkylimos.com",
+    "www.ghdangels.co.uk",
+    "www.jcqualitymanagement.com"
+]
+
+folder('spider-checks') {
+    description('Websites')
+}
+
+for(site in sites){
+    
+    job('spider-checks/' + site) {
+        triggers {
+            cron('H H * * *')
+        }
+        description("Spider link checker: " + site)
+        keepDependencies(false)
+        disabled(false)
+        concurrentBuild(false)
+        steps {
+            shell(
+                """docker run --rm carboninternet/linkchecker """ + 
+                """--threads 1 --recursion-level 3 """ + 
+                """--ignore-url xmlrpc.php\$ """ + site)
+        }
+        publishers {
+            slackNotifier {
+                // teamDomain('foo')
+                // authToken('bar')
+                // room('test')
+                startNotification(false)
+                notifySuccess(false)
+                notifyAborted(true)
+                notifyNotBuilt(true)
+                notifyFailure(true)
+                notifyUnstable(true)
+                notifyBackToNormal(true)
+                notifyRegression(true)
+                notifyRepeatedFailure(true)
+                //includeTestSummary(false)
+                // includeCustomMessage(true)
+                // customMessage('Hello!')
+                // buildServerUrl(null)
+                // sendAs(null)
+                // commitInfoChoice('NONE')
+            }
+        }
+    }
+}
